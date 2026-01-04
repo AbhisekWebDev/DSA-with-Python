0089b0c86138d9aaa8628eb68be95fbc3e9fcd64
Abhisek Kumar <abhi.kumar30091996@gmail.com>
2026-01-04 13:10:14 +0530

    dsa

---
+++ bubbleSort.py
@@ -0,0 +1,25 @@
+def bubbleSort(nums) :
+    
+    for i in range (len (nums)) :
+
+            isSwap = False
+
+            for j in range (len (nums) - i - 1) :
+                
+                if nums[j] > nums[j + 1] :
+                   
+                    # swap
+                    temp = nums[j] 
+                    nums[j] = nums[j + 1] 
+                    nums[j + 1] = temp
+
+                    isSwap = True
+            
+            if not isSwap :
+                break
+        
+    return nums
+
+nums = [1,5,9,7,3,4,6,2,8,0]
+
+print(bubbleSort(nums))
